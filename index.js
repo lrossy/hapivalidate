5c2425ebc8a9b35b72c052fb8ff71e01ecba3af5
Luke Rossy <lrossy@gmail.com>
2014-04-29 22:56:08 -0400

    ...

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ var Joi = require('joi')
 
 // Create a server with a host and port
 var server = Hapi.createServer('0.0.0.0', 8000);
-console.log('here');
 // Add the route
 server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
+  method: 'GET',
+  path: '/hello/{id}',
+  handler: function (request, reply) {
 
-        reply('hello world');
-    },
-config:{
-      validate: {
-        path: {
-          id: Joi.number()
-        }
+    reply('hello world');
+  },
+  config: {
+    validate: {
+      path: {
+        id: Joi.number()
       }
-}
+    }
+  }
 });
 
 // Start the server
